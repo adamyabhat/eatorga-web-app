472e115e2a63f1a0927c4ffa1077ec3bc8995a63
adamyabhat <36527262+adamyabhat@users.noreply.github.com>
2020-08-11 19:45:02 +0530

    inital commit
    EAT ORGA BASIC APP

---
+++ views/src/components/dashboard/OrdersToDeliver.js
@@ -0,0 +1,206 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchOrdersToDeliver } from "../../redux/actions/order-actions/fetchOrdersToDeliver";
+import { markOrderDelivered } from "../../redux/actions/order-actions/markOrderDelivered";
+import { Container, Button, Col, Row, Image, Alert } from "react-bootstrap";
+import { toast, Slide } from "react-toastify";
+import DashboardSidebar from "./DashboardSidebar";
+import { Link } from "react-router-dom";
+import DashboardSpinner from "./DashboardSpinner";
+
+function OrdersToDeliver() {
+  const { ordersToDeliver, loading } = useSelector(state => state.ordersToDeliverrr);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchOrdersToDeliver());
+  }, [dispatch]);
+
+  let emptyMessage;
+  if (!loading && ordersToDeliver.length === 0) {
+    emptyMessage = <Alert variant='warning'>There are no orders yet</Alert>;
+  }
+
+  let loadingSpinner;
+  if (loading && ordersToDeliver.length === 0) {
+    emptyMessage = <DashboardSpinner />;
+  }
+
+  /**
+   * This method to handle our whole update process
+   * it takes the targeted category id and the category object
+   */
+  const handleMarkOrderDelivered = orederId => {
+    //this promise was returned to handle sucess and error messages
+    dispatch(markOrderDelivered(orederId))
+      .then(res => {
+        toast.success(res, {
+          position: toast.POSITION.BOTTOM_LEFT,
+          transition: Slide
+        });
+      })
+      .catch(err => {
+        toast.error(err, {
+          position: toast.POSITION.BOTTOM_LEFT,
+          autoClose: false
+        });
+      });
+  };
+
+  return (
+    <Container fluid className='orders-history'>
+      <Row>
+        <Col md='3'>
+          <DashboardSidebar />
+        </Col>
+        {/*
+         ******************* My orders History *********
+         */}
+        <Col>
+          <h1 className='dashboard-headline'>Orders to deliver</h1>
+          {loadingSpinner}
+          {emptyMessage}
+          {ordersToDeliver.map(order => {
+            let singleOrder = (
+              <Row className='single-order' key={order._id}>
+                <Row className='single-order-heading'>
+                  <Col>
+                    <div className='order-time'>
+                      Order placed <span>{order.orderDate}</span>
+                    </div>
+                  </Col>
+                  <Col>
+                    {order.totalPrice && (
+                      <div className='order-todal-price'>
+                        Total: <span>${order.totalPrice}</span>
+                      </div>
+                    )}
+                  </Col>
+                  <Col md='4'>
+                    <Col>
+                      Recipient:{" "}
+                      <span>
+                        {order.address.firstName + " " + order.address.lastName}
+                      </span>
+                    </Col>
+                    <Col>
+                      Phone: +20<span>{order.address.phoneNumber}</span>
+                    </Col>
+                  </Col>
+                  <Col>
+                    <div className='order-id'>
+                      Order ID: #<span>{order._id}</span>
+                    </div>
+                  </Col>
+                </Row>
+
+                <Row className='order-delivered-time'>
+                  {order.deliveredDate && <div>Delivered on: {order.deliveredDate}</div>}
+                </Row>
+
+                {order.products.map(productItem => (
+                  <Row className='single-order-item' key={productItem._id}>
+                    <Col md='3'>
+                      <Image
+                        src={`${
+                          process.env.PUBLIC_URL +
+                          "/" +
+                          productItem.product.productImage[0].path
+                        }`}
+                        thumbnail
+                      />
+                    </Col>
+
+                    <Col md='9'>
+                      <Row>
+                        <Col md='9'>
+                          <Link to={`/product/${productItem.product._id}`}>
+                            <div>{productItem.product.name}</div>
+                          </Link>
+                          <div className='quantity'>
+                            Quantity: <span>{productItem.quantity}</span>
+                          </div>
+                          <div className='price'>
+                            $Total:
+                            <span>
+                              {" "}
+                              ${productItem.quantity * productItem.product.price}
+                            </span>
+                          </div>
+                        </Col>
+                        <Col md='3'>
+                          {!productItem.orderState.delivered && (
+                            <Button
+                              onClick={() => {
+                                handleMarkOrderDelivered(productItem._id);
+                              }}>
+                              Deliver
+                            </Button>
+                          )}
+
+                          {productItem.orderState.delivered && (
+                            <Button variant='secondary' disabled>
+                              Delivered
+                            </Button>
+                          )}
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                ))}
+              </Row>
+            );
+
+            return singleOrder;
+          })}
+        </Col>
+
+        {/*  <Col>
+          {ordersToDeliver &&
+            ordersToDeliver.map(order => {
+              let singleOrder = (
+                <div className='mb-5' key={order._id}>
+                  <div>Order ID: #{order._id}</div>
+                  <div>Order placed on: {order.orderDate}</div>
+                  {order.totalPrice && <div>Total Price: ${order.totalPrice}</div>}
+                  <div>Address: {order.address.state + " " + order.address.city}</div>
+                  <div>
+                    Recipient: {order.address.firstName + " " + order.address.lastName}
+                  </div>
+                  <div>Phone No.: {order.address.phoneNumber}</div>
+
+                  <Table striped bordered hover variant='dark'>
+                    <thead>
+                      <tr>
+                        <th>Item</th>
+                        <th>quantity</th>
+                        <th>Total Price</th>
+                        <th>Mark as Delivered</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {order.products.map(product => (
+                        <tr key={product._id}>
+                          <td>{product.product.name}</td>
+                          <td>{product.quantity}</td>
+                          <td>{product.product.price * product.quantity}</td>
+                          <td>
+                            
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                </div>
+              );
+
+              return singleOrder;
+            })}
+        </Col> */}
+      </Row>
+    </Container>
+  );
+}
+
+export default OrdersToDeliver;
